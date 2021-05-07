81cf3f15e6d73eca321b8f0ce1cc6b982a943d9a
brakenium <22982712+brakenium@users.noreply.github.com>
2021-05-07 17:08:10 +0200

    Add argument parsing and message formatting

--- commands/dice.js
+++ commands/dice.js
@@ -5,14 +5,26 @@ module.exports = {
     aliases: [], // Aliases for the command
     help: '!dice <amount of sides>; Rolls a dice with x amount of sides. Default of 6. DM', // Help information to show
     restricted: [], // Options: discord ID, role ID, role Name
-    execute(message, args, config) {
+    async execute(message, args, config) {
         message.channel.startTyping();
 
-        // Do the math, pick an inclusive rounded random number between 1 and 6 (default) or a specified number
-        const sides = args[0] || 6;
-        const sideRolled = Math.round(Math.random() * (sides - 1) + 1);
+        let requestedSides = 6;
+        const intParsedInput = parseInt(args[0]);
 
-        message.channel.send(sideRolled);
+        if (args[0] != undefined) {
+            if (Number.isInteger(intParsedInput) && intParsedInput > 0) {
+                requestedSides = intParsedInput;
+            }
+            else {
+                message.channel.stopTyping();
+                return message.channel.send('Incorrect argument');
+            }
+        }
+
+        // Do the math, pick an inclusive rounded random number between 1 and 6 (default) or a specified number
+        const sideRolled = Math.round(Math.random() * (requestedSides - 1) + 1);
+
         message.channel.stopTyping();
+        message.channel.send(`You rolled side **${sideRolled}** out of **${requestedSides}** sides`);
     },
 };
