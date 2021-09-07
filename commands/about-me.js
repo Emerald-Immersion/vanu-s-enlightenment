16477fd3377ec188893546a18f06d6e4b00bd96b
brakenium <22982712+brakenium@users.noreply.github.com>
2021-09-07 14:18:42 +0200

    Fix with new repo

--- commands/about-me.js
+++ commands/about-me.js
@@ -13,10 +13,10 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .setColor()
             .setTitle('About me')
-            .setImage('https://github.com/brakenium/Vanu-s-Enlightenment/raw/master/images/me.png')
+            .setImage('https://github.com/Emerald-Immersion/vanu-s-enlightenment/raw/master/images/me.png')
             .setDescription('I am your friendly neighbourhood connection to Vanu. My work consists of enlightening fools, assisting The Vanu Sovereignty with information and helping followers of Vanu relax among other things.')
             .addField('My physical appearance', 'At the bottom of this message you can see me, in physical form. Sometimes I play with the lovely people in Emerald Immersion and support them wherever I can.')
-            .addField('Misc.', `Currently I have **${commands.length}** commands which can be shown using **!help**. My code resides on a raspberry pi 2 and is licensed under the MIT license. If you want to view my code you can do so on [GitHub](https://github.com/brakenium/Vanu-s-Enlightenment)`);
+            .addField('Misc.', `Currently I have **${commands.length}** commands which can be shown using **!help**. My code resides on a raspberry pi 2 and is licensed under the MIT license. If you want to view my code you can do so on [GitHub](https://github.com/Emerald-Immersion/vanu-s-enlightenment)`);
         message.channel.send(embed);
 
     },
